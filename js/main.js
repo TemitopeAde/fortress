4f29ff0946e51759adcb837918195372807b5d0b
Adesiyan Temitope <adesiyantope2014@gmail.com>
2023-09-28 15:48:33 +0100

    fixed show/hide password on the login page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-const naviagtion = [
-  {
-    text: "Dashboard",
-  },
-  {
-    text: "All users",
-  },
-  {
-    text: "All feedbacks",
-  },
-  {
-    text: "All proposal",
+const password = document.querySelector(".input-div.password input");
+const showBtn = document.querySelector(".login-form-container .show-password button");
+
+showBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log(password.type);
+  switch (password.type) {
+    case "text":
+      password.type = "password"
+      showBtn.textContent = "Show"
+      break;
+      
+    case "password":
+      password.type = "text"
+      showBtn.textContent = "Hide"
+      break;
+  
+    default:
+      break;
   }
-]
+
+  
+})
