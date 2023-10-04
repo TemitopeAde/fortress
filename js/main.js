ec28facb9c1a138014f9a46d50c49958557fdbaa
Adesiyan Temitope <adesiyantope2014@gmail.com>
2023-10-04 19:26:38 +0100

    implented my-proposal

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-const password = document.querySelector(".input-div.password input");
-const showBtn = document.querySelector(".login-form-container .show-password button");
+const password = document?.querySelector(".input-div.password input");
+const showBtn = document?.querySelector(".login-form-container .show-password button");
 
-showBtn.addEventListener('click', (e) => {
+showBtn?.addEventListener('click', (e) => {
   e.preventDefault()
   switch (password.type) {
     case "text":
       password.type = "password"
       showBtn.textContent = "Show"
       break;
-      
+
     case "password":
       password.type = "text"
       showBtn.textContent = "Hide"
       break;
-  
+
     default:
       break;
   }
 
-  
+
 })
 
 
@@ -31,7 +31,7 @@ function isValidEmail(email) {
   return emailRegex.test(email);
 }
 
-document.getElementById("loginForm").addEventListener("submit", function (e) {
+document.getElementById("loginForm")?.addEventListener("submit", function (e) {
   e.preventDefault();
 
   // Get form fields
@@ -54,7 +54,7 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
 
   if (!name.value) {
     nameError.textContent = "Name is required"
-  } 
+  }
 
   if (!phone.value) {
     phoneError.textContent = "Phone number is required"
@@ -87,3 +87,148 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
 
 
 
+const btnProposal = document?.querySelectorAll(".action-btn");
+const actionDiv = document?.querySelectorAll(".action-div-modal")
+
+let lastClickedIndex = null; // Keep track of the last clicked button
+
+btnProposal?.forEach((item, index) => {
+  item.addEventListener("click", (e) => {
+    if (lastClickedIndex !== null) {
+      // Remove the "hidden" class from the old button's actionDiv
+      actionDiv[lastClickedIndex].classList.add("hidden");
+    }
+
+    // Toggle the "hidden" class for the new button's actionDiv
+    actionDiv[index].classList.toggle("hidden");
+
+    // Update the lastClickedIndex to the current index
+    lastClickedIndex = index;
+  })
+})
+
+
+
+
+
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector("#create-proposal");
+const closeModalBtn = document.querySelector(".btn-close");
+
+
+// close modal function
+const closeModal = function () {
+  modal?.classList.add("hidden");
+  overlay?.classList.add("hidden");
+};
+
+// close the modal when the close button and overlay is clicked
+closeModalBtn?.addEventListener("click", closeModal);
+overlay?.addEventListener("click", closeModal);
+
+// close modal when the Esc key is pressed
+document?.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal?.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// open modal function
+const openModal = function () {
+  modal?.classList.remove("hidden");
+  overlay?.classList.remove("hidden");
+};
+// open modal event
+openModalBtn.addEventListener("click", openModal);
+
+
+document.getElementById("file-btn")?.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("myButton").click();
+});
+
+// Listen for changes in the file input
+document.getElementById("myButton")?.addEventListener("change", (e) => {
+  const fileInput = e.target;
+  const fileLabel = document.getElementById("file-label");
+
+  // Check if a file is selected
+  if (fileInput.files.length > 0) {
+    const fileName = fileInput.files[0].name;
+    fileLabel.textContent = fileName;
+  } else {
+    fileLabel.textContent = "Attach Proposal File here";
+  }
+});
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("proposal-form");
+
+  form?.addEventListener("submit", function (event) {
+    const fileInput = document.getElementById("myButton");
+    const fileError = document.getElementById("file-label");
+    const tabOne = document.querySelector(".tab-one")
+    const tabTwo = document.querySelector(".tab-two")
+
+
+
+    if (fileInput.files.length === 0) {
+      fileError.innerHTML = "Please select a file before submitting.";
+      fileError.classList.toggle("error-file")
+      event.preventDefault(); // Prevent form submission
+    } else {
+      fileError.textContent = ""; // Clear any previous error message
+    }
+
+
+    let isValid = true;
+
+    // Reset error messages
+    const errorElements = document.querySelectorAll(".error-text");
+    errorElements.forEach(errorElement => {
+      errorElement.textContent = "";
+    });
+
+    // Validation for Proposal Type 1
+    const type1Select = document.getElementById("select-proposal-type-1");
+    if (type1Select.value === "") {
+      isValid = false;
+      document.getElementById("type-1-error").textContent = "Select Proposal Type";
+    }
+
+    // Validation for Proposal Type 2
+    const type2Select = document.getElementById("select-proposal-type-2");
+    if (type2Select.value === "") {
+      isValid = false;
+      document.getElementById("type-2-error").textContent = "Select user";
+    }
+
+    // Validation for Proposal Title
+    const titleInput = document.getElementById("proposal-title");
+    const titleValue = titleInput.value.trim();
+    if (titleValue === "") {
+      isValid = false;
+      document.getElementById("title-error").textContent = "Proposal Title is required.";
+    }
+
+    // Add more validations for other fields here...
+
+    // Prevent form submission if validation fails
+    if (!isValid) {
+      event.preventDefault();
+    }
+    tabOne.classList.add("hidden")
+    tabTwo.classList.remove("hidden")
+  });
+});
