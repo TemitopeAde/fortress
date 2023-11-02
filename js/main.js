fd985fa65488e47d8db22fc0cc92b651b4a316c5
Adesiyan Temitope <adesiyantope2014@gmail.com>
2023-11-02 14:14:17 +0100

    implented sigining of the docs

--- js/main.js
+++ js/main.js
@@ -262,116 +262,6 @@ feed?.forEach((item) => {
 
 
 
-
-// PDF.JS
-
-
-// If absolute URL from the remote server is provided, configure the CORS
-// header on that server.
-var url = 'resume.pdf';
-
-// Loaded via <script> tag, create shortcut to access PDF.js exports.
-var pdfjsLib = window['pdfjs-dist/build/pdf'];
-
-if (pdfjsLib) {
-
-  // The workerSrc property shall be specified.
-  pdfjsLib.GlobalWorkerOptions.workerSrc = 'js/build/pdf.worker.js';
-
-}
-
-var pdfDoc = null,
-  pageNum = 1,
-  pageRendering = false,
-  pageNumPending = null,
-  scale = 0.8,
-  canvas = document?.getElementById('the-canvas'),
-  ctx = (canvas ? canvas.getContext('2d') : "")
-
-/**
- * Get page info from document, resize canvas accordingly, and render page.
- * @param num Page number.
- */
-function renderPage(num) {
-  pageRendering = true;
-  // Using promise to fetch the page
-  pdfDoc.getPage(num).then(function (page) {
-    var viewport = page.getViewport({ scale: scale });
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-
-    // Render PDF page into canvas context
-    var renderContext = {
-      canvasContext: ctx,
-      viewport: viewport
-    };
-    var renderTask = page.render(renderContext);
-
-    // Wait for rendering to finish
-    renderTask.promise.then(function () {
-      pageRendering = false;
-      if (pageNumPending !== null) {
-        // New page rendering is pending
-        renderPage(pageNumPending);
-        pageNumPending = null;
-      }
-    });
-  });
-
-  // Update page counters
-  // document.getElementById('page_num').textContent = num;
-}
-
-/**
- * If another page rendering in progress, waits until the rendering is
- * finised. Otherwise, executes rendering immediately.
- */
-function queueRenderPage(num) {
-  if (pageRendering) {
-    pageNumPending = num;
-  } else {
-    renderPage(num);
-  }
-}
-
-/**
- * Displays previous page.
- */
-function onPrevPage() {
-  if (pageNum <= 1) {
-    return;
-  }
-  pageNum--;
-  queueRenderPage(pageNum);
-}
-document.getElementById('prev')?.addEventListener('click', onPrevPage);
-
-/**
- * Displays next page.
- */
-function onNextPage() {
-  if (pageNum >= pdfDoc.numPages) {
-    return;
-  }
-  pageNum++;
-  queueRenderPage(pageNum);
-}
-document.getElementById('next')?.addEventListener('click', onNextPage);
-
-/**
- * Asynchronously downloads PDF.
- */
-pdfjsLib?.getDocument(url).promise.then(function (pdfDoc_) {
-  pdfDoc = pdfDoc_;
-  // document.getElementById('page_count').textContent = pdfDoc.numPages;
-
-  // Initial/first page rendering
-  renderPage(pageNum);
-});
-
-
-
-
 feedBackForm?.addEventListener('submit', function (e) {
   // Reset error messages
   type1Error.textContent = '';
@@ -586,31 +476,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
   // Get references to form elements
   const form = document.querySelector('.all-proposal-form');
@@ -673,7 +538,7 @@ function addYearData(year, data) {
   const option = document.createElement('option');
   option.value = year;
   option.textContent = year;
-  yearSelect.appendChild(option);
+  yearSelect?.appendChild(option);
 }
 
 // Example data for two years
@@ -683,7 +548,7 @@ addYearData('2023', [16.4, 19.8, 26.8, 21.4, 40.6, 55.2, 7.4, 9.8, 87.8, 76, 190
 // Wait for the document to fully load
 document.addEventListener('DOMContentLoaded', function () {
   const yearSelect = document.getElementById('yearSelect');
-  const ctx = document.getElementById('myChart').getContext('2d');
+  const ctx = document?.getElementById('myChart')?.getContext('2d');
   let chart = null;
 
   // Function to update the chart based on the selected year
@@ -716,17 +581,25 @@ document.addEventListener('DOMContentLoaded', function () {
           display: true,
           text: `Number of proposals (${selectedYear})`,
         },
-        
+
       },
     });
   }
 
   // Initial chart display for the default year
-  updateChart(yearSelect.value);
+  updateChart(yearSelect?.value);
 
   // Event listener to update the chart when the year selection changes
-  yearSelect.addEventListener('change', (event) => {
+  yearSelect?.addEventListener('change', (event) => {
     const selectedYear = event.target.value;
     updateChart(selectedYear);
   });
 });
+
+
+
+
+
+
+
+
